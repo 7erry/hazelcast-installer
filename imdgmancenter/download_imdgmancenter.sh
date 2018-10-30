69ff78f1914f8de82778b5107fb45fa9730f690b
Terry Walters <terry@hazelcast.com>
2018-10-30 14:10:53 -0400

    added version to the download script

--- imdgmancenter/download_imdgmancenter.sh
+++ imdgmancenter/download_imdgmancenter.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
 #pass in the version to be downloaded ie 3.11
-    wget http://download.hazelcast.com/management-center/hazelcast-management-center-$1.tar.gz
-    tar -xvzf hazelcast-management-center-$1.tar.gz
-    rm -rf ./hazelcast-management-center-$1.tar.gz
-    chmod a+x ./hazelcast-management-center-$1/bin/*sh
+export version=$1
+if [ $# -eq 0 ]
+  then
+    export version=3.11
+fi
+
+    wget http://download.hazelcast.com/management-center/hazelcast-management-center-$version.tar.gz
+    tar -xvzf hazelcast-management-center-$version.tar.gz
+    rm -rf ./hazelcast-management-center-$version.tar.gz
+    chmod a+x ./hazelcast-management-center-$version/bin/*sh
