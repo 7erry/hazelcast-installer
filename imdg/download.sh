e74cec466f96c7e4d2348fb4ca3e9eba06c2884e
Terry Walters <terry@hazelcast.com>
2018-10-30 13:15:44 -0400

    replaced with download_imdg.sh

--- imdg/download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#pass in the version to be downloaded ie 3.11
-    wget http://download.hazelcast.com/enterprise/hazelcast-enterprise-$1.tar.gz
-    tar -xvzf hazelcast-enterprise-$1.tar.gz
-    rm -rf ./hazelcast-enterprise-$1.tar.gz
-    chmod a+x ./hazelcast-enterprise-$1/bin/*sh
