daaed387470d8af2bd098541c9999b42c02ca024
Terry Walters <terry@hazelcast.com>
2018-10-30 14:09:34 -0400

    added version to download script

--- imdg/download_imdg.sh
+++ imdg/download_imdg.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
 #pass in the version to be downloaded ie 3.11
-    wget http://download.hazelcast.com/enterprise/hazelcast-enterprise-$1.tar.gz
-    tar -xvzf hazelcast-enterprise-$1.tar.gz
-    rm -rf ./hazelcast-enterprise-$1.tar.gz
-    chmod a+x ./hazelcast-enterprise-$1/bin/*sh
+export version=$1
+if [ $# -eq 0 ]
+  then
+    export version=3.11
+fi
+
+    wget http://download.hazelcast.com/enterprise/hazelcast-enterprise-$version.tar.gz
+    tar -xvzf hazelcast-enterprise-$version.tar.gz
+    rm -rf ./hazelcast-enterprise-$version.tar.gz
+    chmod a+x ./hazelcast-enterprise-$version/bin/*sh
